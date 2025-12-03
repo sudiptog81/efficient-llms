6619f25ce6ec1f7130ac1729038a15e2942f605a
Sudipto Ghosh <sudipto.ghosh@scai.iitd.ac.in>
2025-12-03 11:26:56 +0530

    fix vertical align

--- src/components/ResearchAreas.js
+++ src/components/ResearchAreas.js
@@ -24,10 +24,10 @@ const ResearchAreas = ({ researchAreas = [] }) => (
 
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto" >
       {researchAreas.map((area, index) => (
-        <Link key={area.title} href={`/areas/${area.slug}`} className="p-6 bg-white dark:bg-gray-900 rounded-xl shadow-lg border border-zinc-100 dark:border-gray-800 transform hover:scale-[1.02] transition duration-300">
+        <Link key={area.title} href={`/areas/${area.slug}`} className="p-6 flex items-center bg-white dark:bg-gray-900 rounded-xl shadow-lg border border-zinc-100 dark:border-gray-800 transform hover:scale-[1.02] transition duration-300">
 
           <div className="flex flex-row align-center items-center gap-4">
-            <div className={`p-3 ${index % 2 === 0 ? 'bg-indigo-500 dark:bg-indigo-400' : 'bg-red-500 dark:bg-red-400'} rounded-lg inline-flex`}
+            <div className={`p-3 ${index % 2 === 0 ? 'bg-indigo-500 dark:bg-indigo-400' : 'bg-red-500 dark:bg-red-400'} rounded-lg`}
             >
               {/* <div className="p-3 bg-indigo-50 dark:bg-gray-800 rounded-lg inline-flex" suppressHydrationWarning> */}
               {(() => {
