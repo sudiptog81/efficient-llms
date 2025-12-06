65abd4e935f2ad59700d5d030e75b523cbf354b9
Sudipto Ghosh <sudipto@ghosh.pro>
2025-12-06 22:52:06 +0530

    remove margin from areas

--- src/components/AreaContent.js
+++ src/components/AreaContent.js
@@ -1,7 +1,7 @@
 const AreaContent = ({ contentHtml }) => (
   <div className="py-2">
     <div
-      className="prose prose-zinc dark:prose-invert max-w-none prose-headings:font-semibold prose-a:text-blue-600 dark:prose-a:text-blue-400  prose-text-base prose-p:leading-6"
+      className="prose prose-zinc dark:prose-invert max-w-none prose-headings:font-semibold prose-a:text-blue-600 dark:prose-a:text-blue-400  prose-text-base prose-p:leading-6 prose-img:mt-0"
       dangerouslySetInnerHTML={{ __html: contentHtml }}
     />
   </div>
